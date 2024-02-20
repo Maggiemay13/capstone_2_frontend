5c83ee6e8af331ed8d557fea3931074e0b2a960c
maggie <margaret13.may@gmail.com>
2024-02-20 17:23:02 -0600

    change baseurl

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:3001";
+const BASE_URL = "https://mindfullmoment.onrender.com";
+// process.env.BASE_URL || "http://localhost:3001";
 
 /** API Class.
  *
